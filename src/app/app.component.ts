7d6c0bc543425208a7dfb966eb0dd66513865386
Vukovic Nikola <admin@nikolav.rs>
2025-01-01 19:05:09 +0100

    init ngapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { Foobar } from './foobar/foobar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Foobar],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngapp';
+export class AppComponent implements OnInit {
+  id = '61b97ab7-e5c1-5697-8182-590fcdfe16fa';
+  title = 'NGApp';
+
+  public ngOnInit(): void {
+    console.log('@ngOnInit');
+  }
 }
---
+++ src/app/foobar/foobar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'foobar',
+  imports: [],
+  templateUrl: './foobar.component.html',
+  styleUrl: './foobar.component.scss',
+})
+export class Foobar {}
