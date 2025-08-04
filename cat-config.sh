3f3e78d66bbca2563d86430333b811d115a296dd
Vukovic Nikola <admin@nikolav.rs>
2025-08-04 14:59:06 +0200

    Mon, Aug  4, 2025 12:59:06 PM

--- cat-config.sh
+++ cat-config.sh
@@ -6,3 +6,4 @@ cat .env
 echo '@@ ./src/app/config/vars.env.ts'
 cat ./src/app/config/vars.env.ts
 
+# find . -type d -name "node_modules" -prune -o -type f -name "*" -exec grep --color=auto -Hn "class" {} +
--- src/app/config/vars.env.public.ts
+++ src/app/config/vars.env.public.ts
@@ -1,10 +1,11 @@
-import { trimEndBase } from "../utils/trim-end-base";
+
+// import { trimEndBase } from "../utils/trim-end-base";
 import { PRODUCTION } from "./vars.env";
 
 export const APP_NAME = "200-ngapp";
 export const ADMIN_EMAIL = "admin@nikolav.rs";
 
-export const URL_APP_PUBLIC_production = "https://nikolav.rs/";
+export const URL_APP_PUBLIC_production = "https://ngapp---iec2cy5qtf---dev.web.app/";
 export const URL_APP_PUBLIC_dev = "http://localhost:4200/";
 export const URL_APP_PUBLIC = PRODUCTION
   ? // "https://qdeiymppite.web.app/"
@@ -19,19 +20,9 @@ export const URL_PRODUCT_PREVIEW_HOST = URL_APP_PUBLIC;
 
 const LOGGING: boolean = true;
 
-export const THEME_ACCENT_SHIFT = 44;
 export const DEBUG = !PRODUCTION && LOGGING;
-
-export const URL_PASSWORD_RESET_FORM_LINK = `${trimEndBase(
-  URL_APP_PUBLIC,
-  "/"
-)}/auth-password-reset-form`;
-
-export const URL_VERIFY_EMAIL = `${trimEndBase(
-  URL_APP_PUBLIC,
-  "/"
-)}/auth-verify-email`;
 
 export { SSR } from "./vars.env";
 
 export const DEFAULT_NO_IMAGE_AVAILABLE = "/no-image.jpg";
+
