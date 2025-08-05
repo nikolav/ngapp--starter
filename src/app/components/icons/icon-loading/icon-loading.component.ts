8029779a9501a556ae9c8e562db8ccd45fb848c6
Vukovic Nikola <admin@nikolav.rs>
2025-08-05 19:04:11 +0200

    Tue, Aug  5, 2025  5:04:11 PM

--- src/app/components/icons/icon-loading/icon-loading.component.ts
+++ src/app/components/icons/icon-loading/icon-loading.component.ts
@@ -7,13 +7,29 @@ import { Component, computed, input } from "@angular/core";
   styleUrl: "./icon-loading.component.scss",
 })
 export class IconLoadingComponent {
+  // ##imports
+  // ##config:const
+  // ##config ##props ##route ##attrs ##form-fields
   size = input<any>("1em");
   width = input(10);
   gap = input(10);
   duration = input(4.1666);
+  // ##schemas
+  // ##utils
+  // ##icons
+  // ##refs ##flags ##models
+  // ##data ##auth ##state
+  // ##computed
   r = computed(() => Math.max(50 - this.width() / 2 - 1, 0));
   darray = computed(
     () => `${2 * Math.PI * this.r() - this.gap()} ${this.gap()}`
   );
   dur = computed(() => `${this.duration()}s`);
+  // ##forms ##handlers ##helpers ##small-utils
+  // ##watch
+  // ##hooks ##lifecycle
+  // ##head ##meta
+  // useHead({ title: "" });
+  // ##provide
+  // ##io
 }
