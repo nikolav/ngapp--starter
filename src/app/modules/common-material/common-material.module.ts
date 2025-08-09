392260ef28ae8a6e4ddc7f7ebd48b269fa82e4fb
Vukovic Nikola <admin@nikolav.rs>
2025-08-09 18:05:38 +0200

    Sat, Aug  9, 2025  4:05:38 PM

--- src/app/modules/common-material/common-material.module.ts
+++ src/app/modules/common-material/common-material.module.ts
@@ -35,9 +35,9 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
 //
-// import {MatDatepickerModule} from '@angular/material/datepicker';
-// import {MatTimepickerModule} from '@angular/material/timepicker';
-// import {MatTreeModule} from '@angular/material/tree';
+// import { MatDatepickerModule } from "@angular/material/datepicker";
+// import { MatTimepickerModule } from "@angular/material/timepicker";
+// import { MatTreeModule } from "@angular/material/tree";
 
 const MODULES = [
   MatAutocompleteModule,
