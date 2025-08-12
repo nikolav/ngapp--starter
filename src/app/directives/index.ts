36227c34f50cc5d765ab0b9269695ca168ed5c6e
Vukovic Nikola <admin@nikolav.rs>
2025-08-12 19:06:52 +0200

    AnimatecssDirective

--- src/app/directives/index.ts
+++ src/app/directives/index.ts
@@ -4,3 +4,4 @@ export { HilightBasicDirective } from "./hilight-basic.directive";
 export { UnlessDirective } from "./unless.directive";
 export { PageLayoutDirective } from "./page-layout.directive";
 export { ClickOutsideDirective } from "./click-outside.directive";
+export { AnimatecssDirective } from "./animate-css.directive";
