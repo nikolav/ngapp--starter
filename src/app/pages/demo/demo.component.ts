688e893a20cf2860464f30c7a816bf3473e472ea
Vukovic Nikola <admin@nikolav.rs>
2025-10-12 20:18:17 +0200

    Sun, Oct 12, 2025  6:18:17 PM

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { transition, trigger, useAnimation } from "@angular/animations";
+
 import { LayoutDefault } from "../../layouts";
 import { MaterialUIModule } from "../../modules";
-import { RouterModule } from "@angular/router";
+import { fade } from "../../assets/animations";
+import { UseToggleFlagService } from "../../services";
 
 @Component({
   selector: "app-demo",
   imports: [LayoutDefault, MaterialUIModule, RouterModule],
   templateUrl: "./demo.component.html",
   styleUrl: "./demo.component.scss",
+  animations: [
+    trigger("show", [
+      transition(
+        ":enter",
+        useAnimation(fade, {
+          params: {
+            opacityFrom: 0,
+            opacityTo: 1,
+            scaleFrom: 0.88,
+            scaleTo: 1,
+            duration: ".24s",
+            easing: "ease-out",
+          },
+        })
+      ),
+      transition(
+        ":leave",
+        useAnimation(fade, {
+          params: {
+            opacityFrom: 1,
+            opacityTo: 0,
+            scaleFrom: 1,
+            scaleTo: 0.88,
+            duration: ".12s",
+            easing: "ease-out",
+          },
+        })
+      ),
+    ]),
+  ],
 })
 export class DemoComponent implements OnDestroy, OnInit {
+  boxIsActive = new UseToggleFlagService();
+
   constructor() {}
 
   ngOnInit() {}
