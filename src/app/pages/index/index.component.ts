97b45d8061ca9b8fe67626ec6cd5b369e59c2d72
Vukovic Nikola <admin@nikolav.rs>
2025-10-12 20:14:32 +0200

    Sun, Oct 12, 2025  6:14:32 PM

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,63 +1,23 @@
-import { Component, OnInit, OnDestroy, inject, effect } from "@angular/core";
+import { Component, OnInit, OnDestroy, effect } from "@angular/core";
 import { Subscription } from "rxjs";
 
 import { LayoutDefault } from "../../layouts";
 import { IconxModule, MaterialUIModule } from "../../modules";
-import {
-  UseCacheKeyService,
-  UseToggleFlagService,
-  UseUtilsService,
-} from "../../services";
+import { UseCacheKeyService } from "../../services";
 import { TOrNoValue } from "../../types/index";
-import { transition, trigger, useAnimation } from "@angular/animations";
-import { fade } from "../../assets/animations";
 
 @Component({
   selector: "page-index",
   imports: [LayoutDefault, MaterialUIModule, IconxModule],
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
-  providers: [],
-  animations: [
-    trigger("show", [
-      transition(
-        ":enter",
-        useAnimation(fade, {
-          params: {
-            opacityFrom: 0,
-            opacityTo: 1,
-            scaleFrom: 0.88,
-            scaleTo: 1,
-            duration: ".24s",
-            easing: "ease-out",
-          },
-        })
-      ),
-      transition(
-        ":leave",
-        useAnimation(fade, {
-          params: {
-            opacityFrom: 1,
-            opacityTo: 0,
-            scaleFrom: 1,
-            scaleTo: 0.88,
-            duration: ".12s",
-            easing: "ease-out",
-          },
-        })
-      ),
-    ]),
-  ],
 })
 export class IndexComponent implements OnInit, OnDestroy {
-  $$ = inject(UseUtilsService);
   $cacheFoo = new UseCacheKeyService().use(
     "cache:c17c95eb-7a5d-5737-b1fa-cfe5f2de5737"
   );
 
   private io_s: TOrNoValue<Subscription>;
-
-  boxIsActive = new UseToggleFlagService();
 
   constructor() {
     effect((onCleanup) => {
