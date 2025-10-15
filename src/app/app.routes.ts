f4d8d37a1bf27ecc3d99c61db97df5bcf94cc367
Vukovic Nikola <admin@nikolav.rs>
2025-10-16 00:49:09 +0200

    Wed, Oct 15, 2025 10:49:09 PM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -45,13 +45,14 @@ export const routes: Routes = [
   //     },
   //   ],
   // },
-  {
-    path: "not-found",
-    component: PageNotFound,
-    data: { animation: "eadf06c1-8885-5364-87f0-256fa0de7c02" },
-  },
+  // {
+  //   path: "not-found",
+  //   component: PageNotFound,
+  //   data: { animation: "eadf06c1-8885-5364-87f0-256fa0de7c02" },
+  // },
   {
     path: "**",
-    redirectTo: "/not-found",
+    redirectTo: "/",
+    // redirectTo: "/not-found",
   },
 ];
