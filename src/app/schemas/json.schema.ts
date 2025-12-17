fab1555f3f3c44929276f806f0bb8c3e07700d74
Vukovic Nikola <admin@nikolav.rs>
2025-12-17 20:31:03 +0100

    Wed, Dec 17, 2025  7:31:03 PM

--- src/app/schemas/json.schema.ts
+++ src/app/schemas/json.schema.ts
@@ -14,14 +14,15 @@ export type TJsonLiteral = JsonLiteral;
 export type TJson = Json;
 export type JsonDataRecord = { [key: string]: Json };
 
-export const schemaJsonData: z.ZodType<Json> = z.lazy(() =>
-  z.union([
-    schemaJsonLiteral,
-    z.array(schemaJsonData),
-    // v4: record(keyType, valueType)
-    z.record(z.string(), schemaJsonData),
-  ])
-);
+// export const schemaJsonData: z.ZodType<Json> = z.lazy(() =>
+//   z.union([
+//     schemaJsonLiteral,
+//     z.array(schemaJsonData),
+//     // v4: record(keyType, valueType)
+//     z.record(z.string(), schemaJsonData),
+//   ])
+// );
+export const schemaJsonData = z.json();
 
 export const schemaJsonDataRecord: z.ZodType<{ [key: string]: Json }> = z.lazy(
   () => z.record(z.string(), schemaJsonData)
