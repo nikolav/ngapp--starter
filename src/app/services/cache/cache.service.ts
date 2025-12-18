4e0b282c8935234c3ec9db49d94142f4ee2017eb
Vukovic Nikola <admin@nikolav.rs>
2025-12-18 15:37:13 +0100

    Thu, Dec 18, 2025  2:37:13 PM

--- src/app/services/cache/cache.service.ts
+++ src/app/services/cache/cache.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from "@angular/core";
-import { map as opMap } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import { Apollo } from "apollo-angular";
 import {
   Q_cacheRedisGetCacheByKey,
@@ -8,18 +8,37 @@ import {
 } from "../../graphql";
 import { UseUtilsService } from "../../services";
 import type {
-  TOrNoValue,
   TRecordJson,
   IResultApolloCacheService,
+  TOrNoValue,
 } from "../../types";
+import { Observable } from "rxjs";
 
 // #https://the-guild.dev/graphql/apollo-angular/docs/get-started#installation
 @Injectable({
   providedIn: "root",
 })
 export class CacheService {
+  readonly ERR_CACHEKEY_EMPTY = "@err:d0c2a135-aaff-5f1f-be19-0a9057991135";
+
   private $apollo = inject(Apollo);
   private $$ = inject(UseUtilsService);
+
+  key$$(cache_key: any) {
+    return new Observable((observer) => {
+      if (!cache_key) {
+        observer.error(this.ERR_CACHEKEY_EMPTY);
+      } else {
+        observer.next(
+          this.$apollo.watchQuery<IResultApolloCacheService>({
+            query: Q_cacheRedisGetCacheByKey,
+            variables: { cache_key },
+          })
+        );
+      }
+      observer.complete();
+    });
+  }
 
   key(cache_key: any) {
     return cache_key
@@ -33,7 +52,7 @@ export class CacheService {
   }
 
   commit(cache_key: any, patch: TOrNoValue<TRecordJson>, merge = true) {
-    return cache_key && !this.$$.isEmpty(patch)
+    return cache_key && (merge ? !this.$$.isEmpty(patch) : patch)
       ? this.$apollo.mutate({
           mutation: M_cacheRedisCommit,
           variables: {
@@ -58,9 +77,7 @@ export class CacheService {
             },
             fetchPolicy: "network-only",
           })
-          .pipe(
-            opMap((res) => this.$$.get(res, "data.cacheRedisDropPathsAtKey"))
-          )
+          .pipe(map((res) => this.$$.get(res, "data.cacheRedisDropPathsAtKey")))
       : this.$$.error$$();
   }
 
