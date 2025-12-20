eefc4cccde28d935428b74dda02b5e542811dfc2
Vukovic Nikola <admin@nikolav.rs>
2025-12-20 19:23:12 +0100

    Sat, Dec 20, 2025  6:23:11 PM

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -11,15 +11,17 @@ import { DocService } from "../../services";
   styleUrl: "./index.component.scss",
 })
 export class IndexComponent implements OnInit, OnDestroy {
-  readonly $d = DocService.init("d:ufPEIPPeweTIX");
+  readonly $d = DocService.init("d:QaHsmyoiFWcWZ4Dlt");
   ok() {
     this.$d
       .commit({
         "x:1": Math.random(),
         "x:2": Math.random(),
       })
-      .subscribe();
-    // this.$d.drop("x:2").subscribe();
+      .subscribe((res) => {
+        console.log({ res });
+      });
+    // this.$d.drop("x:2").subscribe((res) => console.log({ res }));
   }
   ngOnInit() {}
   ngOnDestroy() {}
