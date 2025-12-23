51dca689890eb1fbcc8359fad20da0308807497a
Vukovic Nikola <admin@nikolav.rs>
2025-12-23 15:39:25 +0100

    Tue, Dec 23, 2025  2:39:25 PM

--- src/app/directives/unless.directive.ts
+++ src/app/directives/unless.directive.ts
@@ -1,21 +1,22 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
-  selector: '[appUnless]',
+  selector: "[appUnless]",
 })
 export class UnlessDirective {
-  // directive sits @<ng-template> and has access to template data
-  @Input() set appUnless(value: boolean) {
-    if (!value) {
-      // show view if !value
-      this.vcRef.createEmbeddedView(this.templateRef);
-    } else {
-      this.vcRef.clear();
-    }
-  }
   // can inject template and viewContainer where template is placed
   constructor(
     private templateRef: TemplateRef<any>,
-    private vcRef: ViewContainerRef
+    private vc: ViewContainerRef
   ) {}
+  // directive sits @<ng-template> and has access to template data
+  @Input()
+  set appUnless(value: boolean) {
+    if (!value) {
+      // show view if !value
+      this.vc.createEmbeddedView(this.templateRef);
+    } else {
+      this.vc.clear();
+    }
+  }
 }
