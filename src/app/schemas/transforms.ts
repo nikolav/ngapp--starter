fefc31888416e37807a317f9e91afd7ea2b1b35c
Vukovic Nikola <admin@nikolav.rs>
2025-12-27 17:01:44 +0100

    Sat, Dec 27, 2025  4:01:44 PM

--- src/app/schemas/transforms.ts
+++ src/app/schemas/transforms.ts
@@ -8,5 +8,5 @@ export const transformOverlayOffsets = z
     value ? value.split(/\s+/g).map(Number) : [0, 0]
   )
   .transform((value) =>
-    1 < value.length ? value : [...value, 0, 0].slice(0, 2)
+    (1 < value.length ? value : [...value, 0, 0]).slice(0, 2)
   );
