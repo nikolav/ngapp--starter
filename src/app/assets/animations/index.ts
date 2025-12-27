edfb9965b1ce45ebef82a09cc4eab864defea616
Vukovic Nikola <admin@nikolav.rs>
2025-12-27 22:09:01 +0100

    Sat, Dec 27, 2025  9:09:01 PM

--- src/app/assets/animations/index.ts
+++ src/app/assets/animations/index.ts
@@ -1,4 +1,4 @@
 export { slideInH, slideOutH } from "./slide.animation";
 export { fade } from "./fade.animation";
-export { triggerVisibleHiddenFadeSlide } from "./fade-slide.vs-trigger";
-export { triggerVisibleHiddenFadeScale } from "./fade-scale.vs-trigger";
+export { triggerVisibleHiddenFadeSlide } from "./fade-slide.vh-trigger";
+export { triggerVisibleHiddenFadeScale } from "./fade-scale.vh-trigger";
