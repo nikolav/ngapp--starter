b751876e23b93f92489d8badc6423ad9ded86ca0
Vukovic Nikola <admin@nikolav.rs>
2025-12-27 18:51:27 +0100

    Sat, Dec 27, 2025  5:51:27 PM

--- src/app/schemas/transforms.ts
+++ src/app/schemas/transforms.ts
@@ -3,9 +3,9 @@ import { z } from "zod";
 // #https://zod.dev/api?id=transforms
 export const transformOverlayOffsets = z
   .string()
-  .transform((value: string) =>
-    value ? value.split(/\s+/g).map(Number) : [0, 0]
-  )
-  .transform((value) =>
-    (1 < value.length ? value : [...value, 0, 0]).slice(0, 2)
-  );
+  .trim()
+  .nonempty()
+  .transform((value: string) => {
+    const offsets = value ? value.split(/\s+/g).map(Number) : [0, 0];
+    return (1 < offsets.length ? offsets : [...offsets, 0, 0]).slice(0, 2);
+  });
