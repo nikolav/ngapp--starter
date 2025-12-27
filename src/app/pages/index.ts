d74b3c2c47523339b396fa3de0d00a76bdc2a50b
Vukovic Nikola <admin@nikolav.rs>
2025-12-27 17:52:33 +0100

    Sat, Dec 27, 2025  4:52:33 PM

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,4 +1,1 @@
 export { IndexComponent as PageIndex } from "./index/index.component";
-export { AppComponent as PageApp } from "./app/app.component";
-export { NotFoundComponent as PageNotFound } from "./not-found/not-found.component";
-export { DemoComponent as PageDemo } from "./demo/demo.component";
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -6,9 +6,6 @@ import {
   CoreModulesShared,
 } from "../../modules";
 import { LayoutDefault } from "../../layouts";
-import { PopupConnectedComponent } from "../../components/app";
-import { HostElementDirective } from "../../directives";
-import { triggerFadeSlide } from "../../assets/animations";
 
 @Component({
   selector: "page-index",
@@ -17,12 +14,9 @@ import { triggerFadeSlide } from "../../assets/animations";
     MaterialSharedModule,
     LayoutDefault,
     IconxModule,
-    PopupConnectedComponent,
-    HostElementDirective,
   ],
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
-  animations: [triggerFadeSlide({ name: "fadeSlideUp" })],
 })
 export class IndexComponent implements OnInit, OnDestroy {
   ok() {
