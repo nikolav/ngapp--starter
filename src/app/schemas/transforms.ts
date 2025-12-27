305a960d719d0fcdf0741a7fc2fc3e8c6aba135d
Vukovic Nikola <admin@nikolav.rs>
2025-12-27 19:01:15 +0100

    Sat, Dec 27, 2025  6:01:15 PM

--- src/app/schemas/transforms.ts
+++ src/app/schemas/transforms.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
+import { isNumeric } from "../utils/is-numeric";
 
 // #https://zod.dev/api?id=transforms
 export const transformOverlayOffsets = z
   .string()
   .trim()
-  .transform((value: string) => {
-    // read offsets; number[]
-    const offsets = value ? value.split(/\s+/g).map(Number) : [0, 0];
-    // ensure .length==2; [number, number]
-    return (1 < offsets.length ? offsets : [...offsets, 0, 0]).slice(0, 2);
-  });
+  .transform((value: string) => (value ? value.split(/\s+/g) : [0, 0]))
+  .refine((value) => value.every(isNumeric))
+  .transform((value) => [...value.map(Number), 0, 0].slice(0, 2));
