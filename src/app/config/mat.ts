65bbfcc31c05d35587214b6c04dd427810784c76
Vukovic Nikola <admin@nikolav.rs>
2025-12-28 02:23:28 +0100

    setup breakpoints

--- src/app/config/mat.ts
+++ src/app/config/mat.ts
@@ -1,11 +1,12 @@
 import type { ApplicationConfig } from "@angular/core";
 import { COMMA, ENTER, TAB } from "@angular/cdk/keycodes";
-
 import { MatDateFormats } from "@angular/material/core";
-
 import { MAT_CHIPS_DEFAULT_OPTIONS } from "@angular/material/chips";
 import { MAT_DIALOG_DEFAULT_OPTIONS } from "@angular/material/dialog";
 import { MAT_SELECT_CONFIG } from "@angular/material/select";
+import { Breakpoints } from "@angular/cdk/layout";
+
+import { BreakpointsCustom } from "../assets/breakpoints";
 
 // Custom Date-Formats and Adapter (using https://github.com/iamkun/dayjs)
 export const MAT_DAYJS_DATE_FORMATS: MatDateFormats = {
@@ -29,5 +30,9 @@ export const MAT_DEFAULTS: ApplicationConfig["providers"] = [
   {
     provide: MAT_SELECT_CONFIG,
     useValue: { disableRipple: true, canSelectNullableOptions: true },
+  },
+  {
+    provide: Breakpoints,
+    useValue: BreakpointsCustom,
   },
 ];
