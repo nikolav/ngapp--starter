bea9130172ea48a05edfc6136e251d68c584b26c
Vukovic Nikola <admin@nikolav.rs>
2025-12-30 00:45:52 +0100

    Mon, Dec 29, 2025 11:45:52 PM

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -1,12 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, contentChild, TemplateRef } from "@angular/core";
+
+import { CoreModulesShared } from "../../modules";
 
 @Component({
   selector: "app-layout-default",
-  imports: [],
+  imports: [CoreModulesShared],
   templateUrl: "./default.component.html",
   styleUrl: "./default.component.scss",
   host: {
     class: "layout--default",
   },
 })
-export class DefaultComponent {}
+export class DefaultComponent {
+  readonly slot_page = contentChild("slot_page", { read: TemplateRef });
+  readonly pageData = <any>{};
+}
