6a490ec932a41da32e6e554167b7988ee89a84c6
Vukovic Nikola <admin@nikolav.rs>
2026-01-02 23:34:58 +0100

    Fri, Jan  2, 2026 10:34:58 PM

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -4,9 +4,6 @@ import {
   OnDestroy,
   inject,
   ChangeDetectionStrategy,
-  viewChild,
-  TemplateRef,
-  ViewContainerRef,
 } from "@angular/core";
 
 import {
@@ -15,10 +12,7 @@ import {
   CoreModulesShared,
 } from "../../modules";
 import { LayoutDefault } from "../../layouts";
-
-import { PopupDetachedService, UseUtilsService } from "../../services";
-import { triggerVisibleHiddenFadeSlide } from "../../assets/animations/fade-slide.vh-trigger";
-import { AppFocusCapturedComponent } from "../../components/app";
+import { UseUtilsService } from "../../services";
 
 @Component({
   selector: "page-index",
@@ -27,50 +21,15 @@ import { AppFocusCapturedComponent } from "../../components/app";
     MaterialSharedModule,
     LayoutDefault,
     IconxModule,
-    AppFocusCapturedComponent,
   ],
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [
-    triggerVisibleHiddenFadeSlide({
-      name: "fadeSlideUp",
-    }),
-  ],
 })
 export class IndexComponent implements OnInit, OnDestroy {
-  readonly $vcr = inject(ViewContainerRef);
-
   readonly $$ = inject(UseUtilsService);
-  readonly $popup = inject(PopupDetachedService);
 
-  private tPopup1 = viewChild("popup1", { read: TemplateRef });
-
-  ok() {
-    this.$popup
-      .open({
-        popup1: {
-          target: this.tPopup1()!,
-          overlayOptions: {
-            // centered: true,
-            // fullscreen: true,
-            // point: { x: 122, y: 200 },
-            size: { width: 320, height: "100vh" },
-            top: 0,
-            right: 0,
-            // closeOnEscape: true,
-            // hasBackdrop: true,
-            // closeOnBackdropClick: true,
-          },
-          factoryOptions: {
-            viewContainerRef: this.$vcr,
-          },
-        },
-      })
-      .subscribe((res) => {
-        console.log({ res });
-      });
-  }
+  ok() {}
   ngOnInit() {}
   ngOnDestroy() {}
 }
